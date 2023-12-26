dca995a37de181d3c46fd88b8eeca08f74829a21
IvanSinegovsky <isinegovsky@gmail.com>
2023-12-26 20:17:22 +0300

    added the solution of task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter the number:");
+        int number = scanner.nextInt();
+
+        String numberStr = String.valueOf(number);
+
+        int numberStrLength = numberStr.length();
+
+        String lastDigit = numberStr.substring(numberStrLength - 1);
+
+        System.out.println("Last digit is " + lastDigit);
     }
 }
